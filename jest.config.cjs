ade7e8de9fab6532f91c62b7284577aec9d8ae43
Haris Skiadas <cskiadas@gmail.com>
2024-11-11 23:28:03 -0500

    We are o longer using jest

--- jest.config.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  // preset: 'ts-jest',
-  transform: {
-    '^.+\\.ts$': 'ts-jest',
-  },
-  testEnvironment: 'node',
-  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
-};
